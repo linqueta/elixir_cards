869d551fd097e8b7dcb741aac200b26c8f4e21cd
linqueta <lincolnrodrs@gmail.com>
2019-08-11 20:39:19 -0300

    add suits

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,6 +1,9 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    Enum.map(values, fn x -> Enum.map(suits, fn y -> "#{x} of #{y}" end) end)
   end
 
   def shuffle(deck) do
